2d62fedc9a70df901afccfa1db370db7228f9934
Karim <abdounikarim@gmail.com>
2019-09-03 12:31:40 +0200

    create ImageType and update TrickType to generate a collection of forms

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -6,6 +6,8 @@ use App\Entity\Category;
 use App\Entity\Trick;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,7 +20,13 @@ class TrickType extends AbstractType
             ->add('description')
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'name'
+                'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => false
+            ])
+            ->add('images', CollectionType::class, [
+                'entry_type' => FileType::class,
+                'allow_add' => true
             ])
         ;
     }
