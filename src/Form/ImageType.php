205c7ffc57a53c205d2b129d82b32ef1759657cd
Karim <abdounikarim@gmail.com>
2019-09-04 09:49:46 +0200

    customize form rendering

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,12 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('alt')
-            ->add('file', FileType::class)
+            ->add('alt', TextType::class, [
+                'label' => 'Attribut alt'
+            ])
+            ->add('file', FileType::class, [
+                'label' => 'Fichier'
+            ])
         ;
     }
 
