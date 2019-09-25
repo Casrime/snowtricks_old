1fe988b4301745378c49b8b778262d11f36f952c
Karim <abdounikarim@gmail.com>
2019-09-25 15:02:29 +0200

    add handler service for Trick

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Trick;
-use App\Form\Model\TrickFormModel;
 use App\Form\TrickType;
 use App\Service\FileUploader;
+use App\Service\TrickHandler;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,19 +16,13 @@ class BackController extends AbstractController
     /**
      * @Route("/tricks/new", name="add_trick")
      */
-    public function addTrick(Request $request, FileUploader $fileUploader, EntityManagerInterface $entityManager)
+    public function addTrick(Request $request, TrickHandler $trickHandler)
     {
         $trick = new Trick();
         $form = $this->createForm(TrickType::class, $trick);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()) {
-            foreach ($trick->getImages() as $image) {
-                $filename = $fileUploader->upload($image->getFile());
-                $image->setName($filename);
-                $image->setTrick($trick);
-            }
-            $entityManager->persist($trick);
-            $entityManager->flush();
+            $trickHandler->handle($trick);
         }
         return $this->render('back/trick/new.html.twig', [
             'form' => $form->createView(),
@@ -38,20 +32,12 @@ class BackController extends AbstractController
     /**
      * @Route("/tricks/edit/{id}", name="edit_trick")
      */
-    public function editTrick(Trick $trick, Request $request, FileUploader $fileUploader, EntityManagerInterface $entityManager)
+    public function editTrick(Trick $trick, Request $request, TrickHandler $trickHandler)
     {
         $form = $this->createForm(TrickType::class, $trick);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()) {
-            foreach ($trick->getImages() as $image) {
-                if(!$image->getId()) {
-                    $filename = $fileUploader->upload($image->getFile());
-                    $image->setName($filename);
-                    $image->setTrick($trick);
-                }
-            }
-            $entityManager->persist($trick);
-            $entityManager->flush();
+            $trickHandler->handle($trick);
         }
         return $this->render('back/trick/edit.html.twig', [
             'form' => $form->createView(),
